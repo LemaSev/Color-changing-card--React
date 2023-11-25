fbba549a9561cdf46be66d89c2e5cd47501e34de
lele515 <lemasafi15@gmail.com>
2023-11-25 15:20:37 -0700

    chapter 6- usestate

--- src/Content.js
+++ src/Content.js
@@ -1,28 +1,31 @@
+import { useState} from 'react'
 
 const Content = () => {
+    const [name, setName] = useState('Dave');
+    const [count, setCount] = useState (0);
+
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'Dave'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
     const handleClick = () => {
-        console.log('You Clicked it')
+        setCount(count + 1)
+        console.log(count)
     }
     const handleClick2 = (name) => {
-        console.log(`${name} was Clicked `)
+        console.log(count)
     }
-    const handleClick3 = (e) => {
-        console.log(e.target.innerText)
-    }
+    
 
     return (
         <main>
             <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
+                Hello {name}!
             </p>
+            <button onClick = {handleNameChange}>Change Name</button>
             <button onClick = {handleClick}>Click it</button>
-            <button onClick = {()=> handleClick2('Dave')}>Click it</button>
-            <button onClick = {(e)=> handleClick3(e)}>Click it</button>
+            <button onClick = {handleClick2}>Click it</button>
         </main>
     )
 }
